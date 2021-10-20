e960434fae08e07327c52438b9923a56bedabd5f
AashishVarrey <91568763+AashishVarrey@users.noreply.github.com>
2021-10-20 03:53:19 +0000

    Initial commit

---
+++ Lab4Background.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+import time
+import json
+
+redled = 19
+yellowled = 16
+blueled = 12
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(redled, GPIO.OUT)
+GPIO.setup(yellowled, GPIO.OUT)
+GPIO.setup(blueled, GPIO.OUT)
+
+pwmr = GPIO.PWM(redled, 100)
+pwmy = GPIO.PWM(yellowled, 100)
+pwmb = GPIO.PWM(blueled, 100)
+
+pwmr.start(0)
+pwmy.start(0)
+pwmb.start(0)
+
+while True:
+  with open("Lab4.txt", 'r') as f:
+    data = json.load(f)
+    dutycycle = float(data['ledvalue'])
+    if data['option'] == "red":
+      pwmr.ChangeDutyCycle(dutycycle)
+      pwmy.stop()
+      pwmb.stop()
+    elif data['option'] == "yellow":
+      pwmy.ChangeDutyCycle(dutycycle)
+      pwmr.stop()
+      pwmb.stop()
+    else:
+      pwmb.ChangeDutyCycle(dutycycle)
+      pwmr.stop()
+      pwmy.stop()      
+    time.sleep(0.1)
+
+
